7b61cd1b4cf34be2ab9b57d0c821e62a6194a457
TruongSa <shrimp2t@gmail.com>
2017-05-04 22:47:49 +0700

    Update

--- inc/woocommerce-template-hooks.php
+++ inc/woocommerce-template-hooks.php
@@ -7,4 +7,10 @@
 // Remove star on loop product
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 
+function sshop_add_wishlist_btn(){
+    echo do_shortcode('[yith_wcwl_add_to_wishlist]');
+}
+add_action( 'woocommerce_after_shop_loop_item_title', 'sshop_add_wishlist_btn', 30 );
+
+
 
