c948da4a792bd83e01e1a37d7967fa22799edaf6
TruongSa <shrimp2t@gmail.com>
2017-05-15 21:42:56 +0700

    Add conditional

--- inc/wc-template-functions.php
+++ inc/wc-template-functions.php
@@ -6,26 +6,29 @@
  */
 
 
-/**
- * Change woocommerce get thumbnail html code
- *
- * @see woocommerce_get_product_thumbnail
- */
-function woocommerce_get_product_thumbnail( $size = 'shop_catalog', $deprecated1 = 0, $deprecated2 = 0 ){
-    global $post;
-    $image_size = apply_filters( 'single_product_archive_thumbnail_size', $size );
-    $html = '';
-    if ( has_post_thumbnail() ) {
-        $props = wc_get_product_attachment_props( get_post_thumbnail_id(), $post );
-        $html = get_the_post_thumbnail( $post->ID, $image_size, array(
-            'title'	 => $props['title'],
-            'alt'    => $props['alt'],
-        ) );
-    } elseif ( wc_placeholder_img_src() ) {
-        $html = wc_placeholder_img( $image_size );
-    }
+if ( ! function_exists( 'woocommerce_get_product_thumbnail' ) ) {
+    /**
+     * Change woocommerce get thumbnail html code
+     *
+     * @see woocommerce_get_product_thumbnail
+     */
+    function woocommerce_get_product_thumbnail($size = 'shop_catalog', $deprecated1 = 0, $deprecated2 = 0)
+    {
+        global $post;
+        $image_size = apply_filters('single_product_archive_thumbnail_size', $size);
+        $html = '';
+        if (has_post_thumbnail()) {
+            $props = wc_get_product_attachment_props(get_post_thumbnail_id(), $post);
+            $html = get_the_post_thumbnail($post->ID, $image_size, array(
+                'title' => $props['title'],
+                'alt' => $props['alt'],
+            ));
+        } elseif (wc_placeholder_img_src()) {
+            $html = wc_placeholder_img($image_size);
+        }
 
-    return '<div class="product-thumbnail">'.$html.'</div>';
+        return '<div class="product-thumbnail">' . $html . '</div>';
+    }
 }
 
 
