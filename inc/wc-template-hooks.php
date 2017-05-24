0999ca713990c7897a7aadcc9ddfa9e0581a38c9
TruongSa <shrimp2t@gmail.com>
2017-05-24 22:24:04 +0700

    Update css

--- inc/wc-template-hooks.php
+++ inc/wc-template-hooks.php
@@ -34,13 +34,18 @@ function sshop_add_wishlist_btn(){
 }
 
 // New button position
+/*
 add_action( 'woocommerce_shop_loop_item_btn', 'sshop_add_wishlist_btn', 30 );
 add_action( 'woocommerce_shop_loop_item_btn', 'woocommerce_template_loop_add_to_cart', 10 );
+*/
+//
+add_action( 'woocommerce_shop_loop_item_btn', 'sshop_add_wishlist_btn', 30 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 10 );
 
 
 // Change number product to show
 function sshop_number_products_to_show(){
-    return 24;
+    return 25;
 }
 add_filter( 'loop_shop_per_page','sshop_number_products_to_show', 20 );
 
